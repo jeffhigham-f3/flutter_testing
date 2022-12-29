88f66fca3a067b1f075eec8d41998108f6aa7704
Jeff Higham <jeff@f3code.com>
2022-12-29 12:27:09 -0500

    first commit

---
+++ android/app/src/main/kotlin/com/jeffhigham/flutter/testing/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.jeffhigham.flutter.testing.flutter_testing
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_testing/app/app.dart';
+import 'package:flutter_testing/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
